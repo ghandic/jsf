dedca99d407d35824e628c1c76bc7ea0913b1c14
ghandic <andy.challis@capgemini.com>
2021-04-14 12:29:50 +1000

    incremented version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jsf",
-    version="0.1.1",
+    version="0.2.0",
     author="ghandic",
     description="Creates fake JSON files from a JSON schema",
     long_description=long_description,
