5690351d1e30698b2805b0859979e19d775f162c
ghandic <andy.challis@capgemini.com>
2021-05-03 22:35:42 +1000

    bumping version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jsf",
-    version="0.2.0",
+    version="0.3.0",
     author="ghandic",
     description="Creates fake JSON files from a JSON schema",
     long_description=long_description,
