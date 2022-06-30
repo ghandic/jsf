d10de7de1cf7e8560e659744fdedeed18267056a
ghandic <andrewchallis@hotmail.co.uk>
2022-06-30 13:49:23 +1000

    Releasing v0.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jsf",
-    version="0.3.2",
+    version="0.4.0",
     author="ghandic",
     description="Creates fake JSON files from a JSON schema",
     long_description=long_description,
