dc884a4b6fc26ace1b7c55f0a005a315cc303e0a
Sebastian Rajo <sebastian.rajo@veriff.net>
2024-01-10 12:27:50 +0100

    Fix randint call with floats: failing in Python 3.12

--- jsf/schema_types/string_utils/content_type/text__plain.py
+++ jsf/schema_types/string_utils/content_type/text__plain.py
@@ -5,4 +5,4 @@ import string
 def random_fixed_length_sentence(_min: int = 0, _max: int = 50) -> str:
     if _min > _max:
         raise ValueError("'_max' should be greater than '_min'")  # pragma: no cover
-    return "".join(random.choice(string.ascii_lowercase) for _ in range(random.randint(_min, _max)))
+    return "".join(random.choice(string.ascii_lowercase) for _ in range(random.randint(int(_min), int(_max))))
