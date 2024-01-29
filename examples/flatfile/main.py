713260c9e0b4e0a7fa3da6e06b98d6a66d311bfe
Andy Challis <andy.challis@safetyculture.io>
2024-01-29 15:56:10 +1100

    Fix BUILD for match statement

--- examples/flatfile/main.py
+++ examples/flatfile/main.py
@@ -1,12 +1,13 @@
+import json
 from enum import Enum
 from pathlib import Path
-import json
-import jsonlines
 
-from jsf import JSF
+import jsonlines
 import pandas as pd
 import typer
 from typing_extensions import Annotated
+
+from jsf import JSF
 
 
 class OutputFormat(str, Enum):
@@ -17,7 +18,8 @@ class OutputFormat(str, Enum):
     jsonl = "jsonl"
 
 
-def main(schema: Annotated[
+def main(
+    schema: Annotated[
         Path,
         typer.Option(
             exists=True,
@@ -26,13 +28,13 @@ def main(schema: Annotated[
             writable=False,
             readable=True,
             resolve_path=True,
-            help="Path to the JSON schema used to produce the fake data."
+            help="Path to the JSON schema used to produce the fake data.",
         ),
     ],
     records: Annotated[int, typer.Option(min=0, help="Number of records you wish to produce.")],
     output_format: Annotated[OutputFormat, typer.Option(help="Fake data output format.")],
-    output: Annotated[Path, typer.Option(help="Output file path")]):
-    
+    output: Annotated[Path, typer.Option(help="Output file path")],
+):
     faker = JSF.from_json(schema)
     fake_data = faker.generate(records)
     match output_format:
@@ -40,15 +42,21 @@ def main(schema: Annotated[
             pd.DataFrame.from_records(fake_data).to_csv(output, index=False)
         case OutputFormat.excel:
             more_fake_data = faker.generate(records)
-            custom_header = [v.get("title") or k for k, v in faker.root_schema["properties"].items()]
+            custom_header = [
+                v.get("title") or k for k, v in faker.root_schema["properties"].items()
+            ]
             with pd.ExcelWriter(output) as excel_writer:
-                pd.DataFrame.from_records(fake_data).to_excel(excel_writer, sheet_name='Fake Data', index=False, header=custom_header)
-                pd.DataFrame.from_records(more_fake_data).to_excel(excel_writer, sheet_name='More Fake Data', index=False, header=custom_header)
+                pd.DataFrame.from_records(fake_data).to_excel(
+                    excel_writer, sheet_name="Fake Data", index=False, header=custom_header
+                )
+                pd.DataFrame.from_records(more_fake_data).to_excel(
+                    excel_writer, sheet_name="More Fake Data", index=False, header=custom_header
+                )
         case OutputFormat.json:
             with open(output, "w") as f:
                 json.dump(fake_data, f)
         case OutputFormat.jsonl:
-            with jsonlines.open(output, mode='w') as writer:
+            with jsonlines.open(output, mode="w") as writer:
                 writer.write_all(fake_data)
         case OutputFormat.parquet:
             pd.DataFrame.from_records(fake_data).to_parquet(output, index=False)
