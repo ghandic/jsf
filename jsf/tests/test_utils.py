d76be3bd52b69449c765d36e7253db9bf660887c
Romain Bosa <romain.bosa@gmail.com>
2026-01-13 15:08:39 +0100

    Add test to ensure base_state can be deep copied

--- jsf/tests/test_utils.py
+++ jsf/tests/test_utils.py
@@ -1,4 +1,7 @@
+from copy import deepcopy
+
 import pytest  # pants: no-infer-dep
+from jsf.parser import JSF
 from jsf.schema_types.string import random_fixed_length_sentence
 
 
@@ -10,3 +13,11 @@ def test_random_fixed_length_sentence(_min, _max):
     gen = random_fixed_length_sentence(_min, _max)
     assert len(gen) <= _max
     assert len(gen) >= _min
+
+
+def test_base_state_deepcopy():
+    p = JSF({})
+    assert next(p.base_state["__counter__"]) == 1
+    copied_base_state = deepcopy(p.base_state)
+    assert next(copied_base_state["__counter__"]) == 2
+    assert next(p.base_state["__counter__"]) == 2
